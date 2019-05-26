2197eca5d82b626212d962f5d44fc4897b911458
zmiter <zmiter.mde@gmail.com>
2019-05-26 22:08:16 +0300

    Added CORS configurer

--- src/main/java/com/zmiter/moviestracker/DemoApplication.java
+++ src/main/java/com/zmiter/moviestracker/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -47,6 +49,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/movies").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 }
